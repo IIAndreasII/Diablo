df7e9702d72d99f2168df80cdf508cb093517e85
IIAndreasII <andreas.bartilson@gmail.com>
2017-10-18 09:33:27 +0200

    Worked some on the recursive dungeon-generating algorithm

--- Diablo/Diablo/Dungeon/Dungeon.cs
+++ Diablo/Diablo/Dungeon/Dungeon.cs
@@ -10,7 +10,7 @@ namespace Diablo.Dungeon
     {
         Room[,] myRooms = new Room[10, 10];
 
-        public Dungeon()
+        public Dungeon(int aNumberOfRooms)
         {
             int 
                 tempX = 4,
@@ -33,20 +33,97 @@ namespace Diablo.Dungeon
                     myRooms[tempX, tempY].AddDoor(Doors.RIGHT);
                     break;
             }
-            Generate(myRooms[tempX, tempY]);
+            foreach (Doors item in myRooms[tempX, tempY].GetDoors())
+            {
+                Generate(myRooms[tempX, tempY], item, aNumberOfRooms);
+            }
         }
 
-        private void Generate(Room prevRoom)
+        private void Generate(Room prevRoom, Doors prevDoor, int aNumberOfRooms)
         {
-            /// Recursive motherfucker
+            int
+                tempNumberOfDoors = Utilities.Utility.GetRNG().Next(0, (aNumberOfRooms > 2 ? 2 : aNumberOfRooms));
 
-           
+            if(tempNumberOfDoors <= 0)
+            {
+                switch (prevDoor)
+                {
+                    case Doors.UP:
+                        myRooms[prevRoom.GetXPosition(), prevRoom.GetYPosition() - 1] = new Room(2, 3);
+                        break;
+                    case Doors.DOWN:
+                        myRooms[prevRoom.GetXPosition(), prevRoom.GetYPosition() + 1] = new Room(2, 3);
+                        break;
+                    case Doors.RIGHT:
+                        myRooms[prevRoom.GetXPosition() + 1, prevRoom.GetYPosition()] = new Room(2, 3);
+                        break;
+                    case Doors.LEFT:
+                        myRooms[prevRoom.GetXPosition() - 1, prevRoom.GetYPosition()] = new Room(2, 3);
+                        break;
+                }
+                return;
+            }
+            else
+            {
+                int
+                    tempX = 0,
+                    tempY = 0;
+                Doors
+                    tempPrevDoor = Doors.FAULTYDOOR;
+                switch (prevDoor)
+                {
+                    case Doors.UP:
+                        tempY = -1;
+                        myRooms[prevRoom.GetXPosition(), prevRoom.GetYPosition() + tempY] = new Room(2, 3);
+                        tempPrevDoor = Doors.DOWN;
+                        myRooms[prevRoom.GetXPosition(), prevRoom.GetYPosition() + tempY].AddDoor(tempPrevDoor);
 
+                        break;
+                    case Doors.DOWN:
+                        tempY = 1;
+                        myRooms[prevRoom.GetXPosition(), prevRoom.GetYPosition() + tempY] = new Room(2, 3);
+                        tempPrevDoor = Doors.UP;
+                        myRooms[prevRoom.GetXPosition(), prevRoom.GetYPosition() + tempY].AddDoor(tempPrevDoor);
 
+                        break;
+                    case Doors.RIGHT:
+                        tempX = 1;
+                        myRooms[prevRoom.GetXPosition() + tempX, prevRoom.GetYPosition()] = new Room(2, 3);
+                        tempPrevDoor = Doors.LEFT;
+                        myRooms[prevRoom.GetXPosition() + tempX, prevRoom.GetYPosition()].AddDoor(tempPrevDoor);
 
+                        break;
+                    case Doors.LEFT:
+                        tempX = -1;
+                        myRooms[prevRoom.GetXPosition() + tempX, prevRoom.GetYPosition()] = new Room(2, 3);
+                        tempPrevDoor = Doors.RIGHT;
+                        myRooms[prevRoom.GetXPosition() + tempX, prevRoom.GetYPosition()].AddDoor(tempPrevDoor);
+
+                        break;
+                }
+
+                myRooms[prevRoom.GetXPosition() + tempX, prevRoom.GetYPosition() + tempY].SetPosition(prevRoom.GetXPosition() + tempX, prevRoom.GetYPosition() + tempY);
+
+                for (int i = 0; i < tempNumberOfDoors; i++)
+                {
+                    Doors tempDoor = GetRandomDoor();
+                    while(myRooms[prevRoom.GetXPosition() + tempX, prevRoom.GetYPosition() + tempY].GetDoors().Contains(tempDoor))
+                    {
+                        tempDoor = GetRandomDoor();
+                    }
+                    myRooms[prevRoom.GetXPosition() + tempX, prevRoom.GetYPosition() + tempY].AddDoor(tempDoor);
+                }
+
+                foreach (Doors item in myRooms[prevRoom.GetXPosition() + tempX, prevRoom.GetYPosition() + tempY].GetDoors())
+                {
+                    Generate(myRooms[prevRoom.GetXPosition() + tempX, prevRoom.GetYPosition() + tempY], item, aNumberOfRooms);
+                }
+            }
+
+            
         }
 
-        private List<Doors> GetRandomDoors(Room aPreviousRoom)
+        private List<Doors> GetRandomDoors(Room aPreviousRoom) /// Do I need this?
         {
             List<Doors>
                 tempListToReturn = new List<Doors>();
